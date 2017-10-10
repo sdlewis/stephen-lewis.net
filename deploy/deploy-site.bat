017797c36e18298533c337f7341ddf9e09d9e523
stevielb <iam@stephen-lewis.net>
2017-10-09 23:55:30 -0700

    second pass at deploy script

--- deploy/deploy-site.bat
+++ deploy/deploy-site.bat
@@ -1,4 +1,22 @@
 @echo off 
 
-echo call aws s3 sync %~dp0..\page s3://stevielb.io/ --delete
-call aws s3 sync %~dp0..\page s3://stevielb.io/ --delete
+setlocal
+
+:main
+    if "%~2" EQU "" (goto :usage)
+    
+    set user=%~1
+    set pw=%~2
+    
+    set localdir=%~dp0\
+
+    set cmd=lftp -e "set ssl:verify-certificate no;mirror  ftp://%user%:%pw%@webx15.hostica.com/stephen-lewis.net ;quit" -u %user%,%pw% webx15.hostica.com
+
+    echo %cmd%
+    pause
+    call %cmd%
+
+:usage
+    echo.    %~nx0 ^<username^> ^<password^>
+
+endlocal
